6416f72b7f0cfc92f38a32c22077395d130ec52e
webdevahanaf-ahamad <ahanafahamad1757@gmail.com>
2026-02-06 12:23:40 +0600

    vv

---
+++ js/app.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+    setTimeout(()=>{        
+        const prealoder = document.getElementById('prealoder');
+        prealoder.classList.add('hidden');
+    }, 3000);
+}
