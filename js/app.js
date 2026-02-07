1db292b8eecb3478ba5a5b6f1b879e3eb5d1ace9
webdevahanaf-ahamad <ahanafahamad1757@gmail.com>
2026-02-07 13:46:13 +0600

    new version

--- js/app.js
+++ js/app.js
@@ -2,5 +2,17 @@ window.onload = function(){
     setTimeout(()=>{        
         const prealoder = document.getElementById('prealoder');
         prealoder.classList.add('hidden');
-    }, 3000);
+    }, 2000);
 }
+function menuToggle() {
+    const menubtn = document.getElementById('menu-btn');
+    menubtn.classList.toggle ('icon');
+    const navigation = document.getElementById('navigation');
+    navigation.classList.toggle('change');
+}
+
+window.addEventListener('keydown', (e)=>{
+    if(e.key.toLowerCase() === 'b' && e.ctrlKey){
+        menuToggle()
+    }
+})
